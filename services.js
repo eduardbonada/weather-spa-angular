90e1bcacceca828b0358fc1ecc63441082636493
Eduard Bonada <eduard.bonada@gmail.com>
2016-02-05 09:00:09 +0100

    Move code that fetches weather data into a service

--- services.js
+++ services.js
@@ -4,3 +4,24 @@ weatherApp.service('cityService', function() {
 	this.city = "Barcelona"; // default
 
 });
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+
+	this.getWeather = function(city, days) {
+		// set up the resource (ensuring the browser is not going to complain to get data from another url)
+		var weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily', {
+			callback: 'JSON_CALLBACK'
+		}, {
+			get: {
+				method: 'JSONP'
+			}
+		});
+
+		// get the resource data
+		return weatherAPI.get({
+			q: city,
+			cnt: days,
+			appid: '44db6a862fba0b067b1930da0d769e98'
+		});
+	};
+}]);
