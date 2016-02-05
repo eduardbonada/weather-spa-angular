1c4db6a16e60e91d2143e57d728b2657c877764a
Eduard Bonada <eduard.bonada@gmail.com>
2016-02-05 07:45:06 +0100

    Add days navigation in forecast page

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
 
 	$scope.city = cityService.city;
 
-	$scope.days = $routeParams.days || 2;
+	$scope.days = $routeParams.days || '2';
 
 	// set up the resource (ensuring the browser is not going to complain to get data from another url)
 	$scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily', {
